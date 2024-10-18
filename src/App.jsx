5dd1113033640b12dd41fd7ef9a9203bea0c24c4
ToshpulatovAlimardon <alimardontoshpulatov001@gmail.com>
2024-10-18 19:19:46 +0500

    added apple react

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,12 @@ const App = () => {
           <Chixol image="/2.png" title="Силиконовые" />
           <Chixol image="/3.png" title="Кожаные" />
         </div>
+        <h2>Naushnik</h2>
+        <div className="chixol-wrapper">
+          <Chixol image="/1.png" title="Стеклянные" />
+          <Chixol image="/2.png" title="Силиконовые" />
+          <Chixol image="/3.png" title="Кожаные" />
+        </div>
       </div>
     </>
   );
